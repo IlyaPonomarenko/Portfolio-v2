f5094050ac65d5108f6fd514402960054755d6b3
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-09-15 16:16:19 +0300

    skill section

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const toggle = document.querySelector(".nav-menu")
 const navItems = document.querySelector(".navbar-nav")
+const backButton = document.querySelector('.btn_back_top');
 
     toggle.addEventListener("click", ()=>{
       navItems.classList.toggle("active")
     })
+
+    window.onscroll = function () {
+      scrollFunction();
+    };
+    
+    const scrollFunction = () => {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backButton.style.display = 'block';
+      } else {
+        backButton.style.display = 'none';
+      }
+    };
+
+    const getToTop = () => {
+      console.log('clicked');
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    };
+    
+
+    backButton.addEventListener('click', getToTop);
